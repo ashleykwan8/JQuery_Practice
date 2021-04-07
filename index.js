5333e32b7537148ff9587e18a63294fecfaaafc3
Ashley Kwan <ashleykwan888@gmail.com>
2021-04-07 16:39:20 -0700

    adding classes to tags

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 // jQuery for selecting h1 and changing the css color property to red
-$("h1").css("color", "white");
+// $("h1").css("color", "white");
+
+// this will select all the buttons on the HTNL
+$("button");
+
+// TODO: Keep CSS only to style 
+
+
+// added the class to the h1 tag and changed the behavior of the styling
+$("h1").addClass("big-title");
+// removes the class from h1
+$("h1").removeClass("big-title");
